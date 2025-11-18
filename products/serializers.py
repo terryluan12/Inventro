c0aee7ce9509f84efc33bb10a4dca79f8f15801b
Libritor <51269250+Libritor@users.noreply.github.com>
2025-11-18 15:37:57 -0500

    Update serializers.py

--- products/serializers.py
+++ products/serializers.py
@@ -1,7 +1,17 @@
 from rest_framework import serializers
-from .models import Item
+from .models import Item, ItemCategory
+
+class ItemCategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ItemCategory
+        fields = ("id", "name")
 
 class ItemSerializer(serializers.ModelSerializer):
+    category = ItemCategorySerializer(read_only=True)
+    category_id = serializers.PrimaryKeyRelatedField(
+        queryset=ItemCategory.objects.all(), source="category", write_only=True
+    )
+
     class Meta:
         model = Item
-        fields = ['id', 'SKU', 'name', 'in_stock', 'total_amount', 'category']
+        fields = ("id", "SKU", "name", "in_stock", "total_amount", "category", "category_id")
